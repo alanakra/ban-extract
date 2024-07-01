78ae65b51e3b16eb6d2148816fb08518cff5c5ce
Alan Akra <alanakra@hotmail.fr>
2024-07-01 21:17:46 +0200

    prefixs in set

--- sort-csv-optimized.py
+++ sort-csv-optimized.py
@@ -2,11 +2,11 @@ import os
 import pandas as pd
 import pprint
 
-if not os.path.exists('sorted-voies'):
-    os.makedirs('sorted-voies')
+if not os.path.exists('sorted-voies-cut'):
+    os.makedirs('sorted-voies-cut')
 
-if not os.path.exists('sorted-communes'):
-    os.makedirs('sorted-communes')
+if not os.path.exists('sorted-communes-cut'):
+    os.makedirs('sorted-communes-cut')
 
 def toSort(dpt):
     dpt = f'{int(dpt):02d}'
@@ -16,6 +16,8 @@ def toSort(dpt):
     communes = set()
     
     df = pd.read_csv(file_path, sep=';', usecols=[4, 5, 7], names=['voie', 'post', 'commune'], skiprows=1)
+
+    prefix = {"Boule lyonnaise ", "Boulodrome ", "Boulodrome couvert ", "Centre nautique ", "Centre Sportif ", "City Stade ", "Complexe ", "Complexe sportif ", "Dojo ", "École De Danse ", "Espace ", "Halle ", "Halle des sports ", "Halle sportive ", "Gymnase ", "Gymnase scolaire ", "Jeu de boules ", "Le centre ", "Mini Football ", "Palais des Sports ", "Piscine ", "Piscine municipale ", "Piste d'athlétisme ", "Piste d'athletisme ", "Plateau Sportif ", "Plateaux sportifs ", "Salle ", "Salle de boxe ", "Salle de sport ", "Salle de sports ", "Salle omnisports ", "Skate-Park ", "Square ", "Stade ", "Stade municipal ", "Tennis Club ", "Tennis Club municipal ", "Terrain ", "Terrain de football ", "Terrain de proximité ", "Vélodrome ", ".*Vélodrome ","Crèche( Municipale| PMI| public| privé)* ", "Collège( | public| privé)* ", "École ([ÉE]l[eé]mentaire|maternelle|primaire|technique|technologique)*( |Centre |d'application )*(privée|publique)*[ ]*", "Espace ", "Groupe scolaire ", "Institut ", "Institution ", "Lycée( | général| général et technologique| polyvalent| professionnel| professionnel| technologique| [Tt]echnique)*( | et technologique)*( | public| privé|.*restauration)* ", "PMI ","Biblioth[èe]que( | centrale| communale| départementale| municipale| universitaire)* ", "M[ée]diath[èe]que( | centrale| communale| départementale| municipale)* ","All[ée]e ", "Avenue ", "Boulevard ", "Chemin ", "Cours ", "Galerie ", "Impasse ", "Jardin ", "Mail " , "Passage " , "Place ", "Quai ", "R[ée]sidence ", "Rue ","Ruelle ", "Sente ", "Sentier ", "Square ", "Villa "}
 
     for _, row in df.iterrows():
         voie = row['voie']
@@ -31,12 +33,12 @@ def toSort(dpt):
     voies = sorted(voies)
     communes = sorted(communes)
 
-    with open(f'sorted-voies/sorted-voies-list-{dpt}.py', 'w', encoding='utf-8') as newSortedDptFile:
+    with open(f'sorted-voies-cut/sorted-voies-cut-list-{dpt}.py', 'w', encoding='utf-8') as newSortedDptFile:
         newSortedDptFile.write(pprint.pformat(voies))
-    with open(f'sorted-communes/sorted-communes-list-{dpt}.py', 'w', encoding='utf-8') as newSortedCommunesFile:
+    with open(f'sorted-communes-cut/sorted-communes-cut-list-{dpt}.py', 'w', encoding='utf-8') as newSortedCommunesFile:
         newSortedCommunesFile.write(pprint.pformat(communes))
 
-for x in range(1, 11):
-    if x == 20:
-        continue
-    toSort(x)
+# for x in range(1, 11):
+#     if x == 20:
+#         continue
+toSort(34)
