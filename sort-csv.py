e7551ab0f5d7c4d949569f84149d2156a41a9e93
Alan Akra <alanakra@hotmail.fr>
2024-10-24 09:42:33 +0200

    Remove unoptimized script

--- sort-csv.py
+++ sort-csv.py
@@ -1,6 +1,7 @@
 import os
-import csv
+import pandas as pd
 import pprint
+import datetime
 
 if not os.path.exists('sorted-voies'):
     os.makedirs('sorted-voies')
@@ -9,39 +10,35 @@ if not os.path.exists('sorted-communes'):
     os.makedirs('sorted-communes')
 
 def toSort(dpt):
+    dpt = f'{int(dpt):02d}'
 
-    if dpt < 10:
-        dpt = f'{int(dpt):02d}'
+    file_path = f'tosort/adresses-{dpt}.csv'
+    voies = set()
+    communes = set()
 
-    str(dpt)
+    current_time = datetime.datetime.now()
+    
+    df = pd.read_csv(file_path, sep=';', usecols=[4, 5, 7], names=['voie', 'post', 'commune'], skiprows=1, encoding='utf-8')
 
-    with open(f'tosort/adresses-{dpt}.csv', newline='', encoding='utf-8') as csvfile:
-        csvreader = csv.reader(csvfile)
+    for _, row in df.iterrows():
+        voie = row['voie']
+        commune = row['commune']
+        post = str(row['post']).zfill(5)
+        voies.add((voie, commune))
+        communes.add(commune)
+        print('---')
+        print('Voie:', voie)
+        print('Commune:', commune)
+        print('Post:', post)
 
-        next(csvreader)
+    voies = sorted(voies)
+    communes = sorted(communes)
 
-        voies = []
-        communes = []
-        
-        for i, row in enumerate(csvreader):
-            voie = row[0].split(';')[4]
-            commune = row[0].split(';')[7]
-            post = row[0].split(';')[5]
-            merged = [voie, commune]
-            if not merged in voies:
-                voies.append(merged)
-            if not commune in communes:
-                communes.append(commune)
-            print('---')
-            print('Voie: ' + voie)
-            print('Commune: ' + commune)
-            print('Post: ' + post)
-        
-        communes = sorted(communes)
-
-    with open(f'sorted-voies/sorted-voies-list-{str(dpt)}.py', 'w', encoding='utf-8') as newSortedDptFile:
+    with open(f'sorted-voies/sorted-voies-list-{dpt}.py', 'w', encoding='utf-8') as newSortedDptFile:
+        newSortedDptFile.write(f'# Generated at {current_time} \n')
         newSortedDptFile.write(pprint.pformat(voies))
-    with open(f'sorted-communes/sorted-communes-list-{str(dpt)}.py', 'w', encoding='utf-8') as newSortedCommunesFile:
+    with open(f'sorted-communes/sorted-communes-list-{dpt}.py', 'w', encoding='utf-8') as newSortedCommunesFile:
+        newSortedCommunesFile.write(f'# Generated at {current_time} \n')
         newSortedCommunesFile.write(pprint.pformat(communes))
 
 for x in range(1, 96):
